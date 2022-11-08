cbace447fcbfa7620bfebecda59ac1330210c189
yonatan <yonycherkos@gmail.com>
2022-11-08 04:57:57 +0300

    Implement dio caching for offline use first step

--- lib/repository/pets_api_repository.dart
+++ lib/repository/pets_api_repository.dart
@@ -1,25 +1,49 @@
 import 'package:dio/dio.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+import 'package:dio_cache_interceptor_hive_store/dio_cache_interceptor_hive_store.dart';
 import 'package:figozo_fl_practical/model/pet_info.dart';
+import 'package:path_provider/path_provider.dart';
 
 class PetsApiRepository {
-  Dio? dio;
+  Dio? _dio;
   final baseUrl = 'https://mocki.io/v1';
 
   PetsApiRepository() {
-    if (dio == null) {
+    if (_dio == null) {
       BaseOptions options = BaseOptions(
         baseUrl: baseUrl,
         receiveDataWhenStatusError: true,
         connectTimeout: 60 * 1000, // 60 seconds
         receiveTimeout: 60 * 1000, // 60 seconds
       );
-      dio = Dio(options);
+      _dio = Dio(options);
+      initChache();
     }
+  }
+
+  Future<void> initChache() async {
+    var cacheDir = await getApplicationDocumentsDirectory();
+    var cacheStore = HiveCacheStore(cacheDir.path, hiveBoxName: 'pets');
+    var customCacheOptons = CacheOptions(
+      store: cacheStore,
+      policy: CachePolicy.forceCache,
+      priority: CachePriority.high,
+      maxStale: const Duration(days: 7),
+      hitCacheOnErrorExcept: [401, 404],
+      keyBuilder: (request) {
+        return request.uri.toString();
+      },
+      allowPostMethod: false,
+    );
+
+    _dio!.interceptors.add(
+      DioCacheInterceptor(options: customCacheOptons),
+    );
   }
 
   Future<List<PetInfo>> fetchCatsList() async {
     try {
-      var response = await dio!.get('/77290441-d8e9-483f-80a2-a336a3364bbe');
+      var response = await _dio!.get('/77290441-d8e9-483f-80a2-a336a3364bbe');
       var catsList = <PetInfo>[];
       if (response.statusCode == 200) {
         final catsJson = response.data['cats'];
@@ -35,7 +59,7 @@ class PetsApiRepository {
 
   Future<List<PetInfo>> fetchDogsList() async {
     try {
-      var response = await dio!.get('/442a4801-5715-474a-b6fd-7548ab2f8d20');
+      var response = await _dio!.get('/442a4801-5715-474a-b6fd-7548ab2f8d20');
       var dogsList = <PetInfo>[];
       if (response.statusCode == 200) {
         final dogsJson = response.data['dogs'];
